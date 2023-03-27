1d31bcf02ba102d71abc6c3146213feb69f8f107
Larissa Barra <lari_conde94@hotmail.com>
2023-03-27 21:16:37 +0100

    Initial Commit

---
+++ Converto/ConvertoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertoApp.swift
+//  Converto
+//
+//  Created by Larissa Barra on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConvertoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
